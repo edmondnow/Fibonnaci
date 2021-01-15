1d96b0ef277c6905ced229cdbfd2783eb92e0239
Ednerd <edmond.bitay@gmail.com>
2021-01-15 17:53:04 +0100

    Add grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {grid: null}
+  }
+
+
+
+
+
+  makeGrid = function() {
+
+    let grid = []
+    while(grid.length < 10) {
+      grid.push([]);
+    }
+
+    grid.forEach(el => 	{
+      while (el.length < 10) {
+        el.push([]);
+      }
+    })
+    console.log(grid)
+    this.setState({grid})
+}
+
+componentDidMount() {
+  this.makeGrid();
+}
+
+
+
+render() {
+
+  const size = 50;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="container">
+      <svg width={size * 10} height={size * 10}>
+      {this.state.grid 
+        && this.state.grid.map((row, idxRow) => 
+          <g>
+            {row.map((el, idxCol) => <rect width={size} height={size} x={size*idxRow} y={size*idxCol} fill="gray"></rect>)}
+          </g>
+        )}     
+      </svg>
       </header>
     </div>
   );
-}
+}}
 
 export default App;
