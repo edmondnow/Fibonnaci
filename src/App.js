3749c40e80f56ff127178097106b03edf514a241
Ednerd <edmond.bitay@gmail.com>
2021-01-16 14:36:57 +0100

    Add fibonnaci number

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ class App extends React.Component {
 
 
 
+  fibonnaciNum = function (num, fib = 1, lastFib = 0 ) {
+    if (fib < num ) return this.fibonnaciNum(num, fib + lastFib, fib);
+    if (fib === num)  return true;
+    return false;
+  }
+
 
 
   makeGrid = function() {
@@ -35,19 +41,43 @@ class App extends React.Component {
     this.setState({grid})
 }
 
+createArray = function (arr1, arr2) {
+  if (!arr1.length < 10) return arr1;
+  arr2.forEach(el => arr1.push(el)) 
+}
+
+
+deconstruct = function(grid) {
+  let sequences = grid; 
+  let columns = [];
+
+  
+
+  while (columns.length < 10) {
+      let column;
+      grid.forEach(el => column.push(el))
+  }
+
+}
+
+
+checkGrid = function(grid) {
+  
+  this.setState({grid});
+}
+
 onClick = function(idxCol, idxRow, e) {
     let { grid } = this.state;
-    console.log( "r",idxRow, "c", idxCol )
-    console.log(idxCol)
+
+    grid[idxCol].forEach((el, idx, arr) =>  {
+      if (idxRow !== idx)  arr[idx]++
+    }
     
-    grid[idxCol].forEach((el, idx, arr) => 
-      arr[idx]++
     );
     
     grid.forEach(el => el[idxRow]++)
-    this.setState({grid})
+    this.checkGrid(grid)    
 }
-
 componentDidMount() {
   this.makeGrid();
 }
@@ -69,7 +99,7 @@ render() {
             <g>
             <rect className="rect" onClick={(e) => this.onClick(idxCol, idxRow, e)} width={cellSize} height={cellSize} x={cellSize * idxRow} y={cellSize * idxCol} fill="gray" /> 
             <text x={cellSize*idxRow} y={(cellSize * idxCol) + cellSize } font-family="Verdana" fontSize="20" fill="blue">
-              {`e${el}r${idxRow}c${idxCol}`} </text></g>)}
+              {`${el}`} </text></g>)}
           </g>
         )}     
       </svg>
